7b9b94cda4da7b08c2c717767706fca58676546d
Erfy <jan.vvojnar@gmail.com>
2023-06-14 22:48:05 +0200

    Message priority tested with default priority

--- JmsFundamentals/src/main/java/com/jawojnar/jms/messagestructure/MessagePriority.java
+++ JmsFundamentals/src/main/java/com/jawojnar/jms/messagestructure/MessagePriority.java
@@ -18,25 +18,33 @@ public class MessagePriority {
                 JMSContext jmsContext = cf.createContext();
         ){
             JMSProducer producer = jmsContext.createProducer();
-            String[] messages = new String[3];
+            String[] messages = new String[4];
             messages[0] = "Message One";
             messages[1] = "Message Two";
             messages[2] = "Message Three";
+            messages[3] = "Message with default priority";
+
+            producer.send(queue,messages[3]);
 
             producer.setPriority(3);
             producer.send(queue,messages[0]);
 
-            producer.setPriority(1);
+            producer.setPriority(4);
             producer.send(queue,messages[1]);
 
             producer.setPriority(9);
             producer.send(queue,messages[2]);
 
+
             JMSConsumer consumer = jmsContext.createConsumer(queue);
 
-            for(int i = 0 ; i<3 ; i++) {
-                System.out.println(consumer.receiveBody(String.class));
+            for(int i = 0 ; i<4 ; i++) {
+                Message receivedMessage = consumer.receive(5000L);
+                System.out.println("Message priority: " + receivedMessage.getJMSPriority());
+                System.out.println("Message body text: " + receivedMessage.getBody(String.class) + "\n");
             }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
         }
 
 
