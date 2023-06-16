517795b62fbe3d259ec841ea36e0d896bd752300
Jan Wojnar <jan.wojnar@capgemini.com>
2023-06-16 23:49:14 +0200

    MessageExpirationDemo

---
+++ JmsFundamentals/src/main/java/com/jawojnar/jms/messagestructure/MessageExpirationDemo.java
@@ -0,0 +1,34 @@
+package com.jawojnar.jms.messagestructure;
+
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageExpirationDemo {
+
+    public static void main(String[] args) throws NamingException {
+
+        InitialContext context = new InitialContext();
+        Queue queue = (Queue) context.lookup("queue/myQueue");
+
+        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory()) {
+            JMSContext jmsContext = cf.createContext();
+
+            JMSProducer producer = jmsContext.createProducer();
+            producer.setTimeToLive(2000L);
+            producer.send((Destination) queue, "Ugghhhh");
+            Thread.sleep(5000);
+
+            Message messageReceived = jmsContext.createConsumer((Destination) queue).receive(5000L);
+            System.out.println(messageReceived);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
