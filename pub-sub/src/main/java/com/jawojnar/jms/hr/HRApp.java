eb311ba4c53e516139a0111f3fc68e0482938469
Erfy <jan.vvojnar@gmail.com>
2023-06-25 17:02:55 +0200

    pub-sub: sending msg to topic

---
+++ pub-sub/src/main/java/com/jawojnar/jms/hr/HRApp.java
@@ -0,0 +1,32 @@
+package com.jawojnar.jms.hr;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import javax.jms.JMSContext;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class HRApp {
+    public static void main(String[] args) throws NamingException {
+
+        InitialContext context = new InitialContext();
+        Topic topic = (Topic) context.lookup("topic/empTopic");
+
+        try (
+                ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+                JMSContext jmsContext = cf.createContext();
+        ){
+            Employee employee = new Employee();
+            employee.setId(123);
+            employee.setFirstName("Jan");
+            employee.setLastName("Wojnar");
+            employee.setDesignation("Software Architect");
+            employee.setEmail("jan.wojnar@gmail.com");
+            employee.setPhone("123123123");
+
+            jmsContext.createProducer().send(topic,employee);
+            System.out.println("Message sent!");
+        }
+    }
+}
